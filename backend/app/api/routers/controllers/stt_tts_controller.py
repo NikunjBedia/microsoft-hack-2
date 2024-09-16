46f63ff7c2e1ef266dc5de17adfe0a3e96979aeb
chirayurathi <chirayurathi4@gmail.com>
2024-09-16 23:01:30 +0530

    added axios

--- backend/app/api/routers/controllers/stt_tts_controller.py
+++ backend/app/api/routers/controllers/stt_tts_controller.py
@@ -5,9 +5,9 @@ import io
 from elevenlabs.client import ElevenLabs
 from pydantic import BaseModel
 from faster_whisper import WhisperModel
-from app.core.agent.core import SuperGraph
+#from app.core.agent.core import SuperGraph
 
-super_graph = SuperGraph()
+#super_graph = SuperGraph()
 stt_tts_router = APIRouter()
 
 class GraphRequest(BaseModel):
@@ -57,14 +57,9 @@ async def stt_tts_handler(websocket: WebSocket, request: GraphRequest):
             
 
             #Get response from graph
-            if request.is_initial:
-                result = super_graph.start_conversation(user_text)
-            elif super_graph.awaiting_feedback():
-                result = super_graph.continue_with_human_feedback(user_text)
-            else:
-                result = super_graph.handle_interruption(user_text)
+           
             
-            response_text = result #TODO:Check result object dictionary to get the response text
+            response_text = "result "#TODO:Check result object dictionary to get the response text
             await websocket.send_text(response_text) # Model response text
             
             # Generate audio response using ElevenLabs
